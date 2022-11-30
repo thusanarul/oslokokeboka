e305447e30ef9c8b70c1e60c2f121c5d62fad6d9
Thusan Arul <thusan.arul@iterate.no>
2022-11-30 21:32:42 +0100

    Creates recipe route, and starts building recipe preview component

---
+++ remix-oslokokeboka/app/routes/_internal/recipe.$id.tsx
@@ -0,0 +1,97 @@
+import { InputType } from "@prisma/client";
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { db } from "~/utils/db.server";
+
+// create recipe layout from json?
+
+type Recipe = Record<
+  string,
+  {
+    index: string;
+    name: string;
+    inputType: InputType;
+    inputValue: string;
+  }
+>;
+
+export const loader: LoaderFunction = async ({ params }) => {
+  invariant(params.id, "id is required to fetch recipe");
+
+  const recipeFields = await db.recipeField.findMany({
+    where: {
+      recipeSubmissionId: params.id,
+      RecipeSubmission: {
+        completed: true,
+      },
+    },
+    select: {
+      index: true,
+      name: true,
+      inputType: true,
+      inputValue: true,
+    },
+    orderBy: {
+      index: "asc",
+    },
+  });
+
+  const recipe: Recipe = recipeFields.reduce((obj, item) => {
+    return {
+      ...obj,
+      [item.name]: item,
+    };
+  }, {});
+
+  return json(recipe);
+};
+
+export default function Recipe() {
+  const recipe: Recipe = useLoaderData();
+
+  console.log(recipe);
+
+  return (
+    <div className="w-full h-screen">
+      <section id="recipe" className="flex flex-col w-[90%] bg-white mx-auto">
+        <InfoBox recipe={recipe} />
+      </section>
+      <div></div>
+    </div>
+  );
+}
+
+const InfoBox = ({ recipe }: { recipe: Recipe }) => {
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <h1 className="text-paper">{recipe["name-of-dish"].inputValue}</h1>
+      </span>
+      <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-paper text-[17px]">
+          {recipe["who-are-you"].inputValue}
+        </p>
+      </span>
+      <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-salmon text-[17px]">
+          {recipe["who-are-you"].inputValue}
+        </p>
+      </span>
+      <dl className="flex justify-between bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <span>
+          <dt className="text-salmon text-[12px]">Type</dt>
+          <dl className="text-paper text-[17px]">
+            {recipe["what-kind-of-dish"].inputValue}
+          </dl>
+        </span>
+        <span>
+          <dt className="text-salmon text-[12px]">Serves</dt>
+          <dl className="text-paper text-[17px]">
+            {recipe["how-many-servings"].inputValue}
+          </dl>
+        </span>
+      </dl>
+    </div>
+  );
+};
