4156231ed7eae21fd3ff3bbb683a6b8d46cb2838
Thusan Arul <thusan.arul@iterate.no>
2022-12-06 21:44:09 +0100

    Skeleton of recipe component

--- remix-oslokokeboka/app/routes/_internal/recipe.$id.tsx
+++ remix-oslokokeboka/app/routes/_internal/recipe.$id.tsx
@@ -50,20 +50,110 @@ export const loader: LoaderFunction = async ({ params }) => {
 export default function Recipe() {
   const recipe: Recipe = useLoaderData();
 
-  console.log(recipe);
-
   return (
     <div className="w-full h-screen">
       <section
         id="recipe"
-        className="flex flex-col w-[90%] max-w-[540px] bg-white mx-auto"
+        className="flex flex-col w-[90%] max-w-[540px] bg-white mx-auto gap-[10px]"
       >
         <InfoBox recipe={recipe} />
+        <AuthorSays recipe={recipe} />
+        <Ingredients recipe={recipe} />
+        <HowTo recipe={recipe} />
+        <Pictures recipe={recipe} />
+        <AdditionalInfo recipe={recipe} />
       </section>
       <div></div>
     </div>
   );
 }
+
+const AdditionalInfo = ({ recipe }: { recipe: Recipe }) => {
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <span className="bg-darkwine py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[15px]">Additonal info</p>
+      </span>
+      <span className="bg-darkwine py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[15px]">{recipe["additional-notes"].inputValue}</p>
+      </span>
+    </div>
+  );
+};
+
+const Pictures = ({ recipe }: { recipe: Recipe }) => {
+  // Need to implement in form first anyways
+  return null;
+};
+
+const HowTo = ({ recipe }: { recipe: Recipe }) => {
+  const parsed = recipe["how-to"].inputValue.split("\n");
+
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <span className="bg-darkwine py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[15px]">Do the following</p>
+      </span>
+      <span className="bg-darkwine py-[20px] pl-[20px] pr-[40px]">
+        {parsed.map((value, index) => {
+          if (!value) {
+            return;
+          }
+          return (
+            <p key={`how_to-${index}`} className="text-[15px]">
+              {value}
+            </p>
+          );
+        })}
+      </span>
+    </div>
+  );
+};
+
+const Ingredients = ({ recipe }: { recipe: Recipe }) => {
+  const ingredients = recipe["ingredients"].inputValue.split(";");
+
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <span className="bg-darkwine py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[15px]">To make this, you will need</p>
+      </span>
+      {ingredients.map((val, index) => {
+        const ingredient = val.split(":");
+
+        if (ingredient.length != 3) {
+          return;
+        }
+
+        return (
+          <div
+            key={`ingredient-${index}`}
+            className="flex  bg-darkwine py-[20px] pl-[20px] pr-[40px] justify-between"
+          >
+            <p className="text-[17px] text-paper">{ingredient[0]}</p>
+            <span className="flex">
+              <p className="text-[17px] text-paper">{ingredient[1]}</p>
+              <p className="text-[17px] text-paper">{ingredient[2]}</p>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const AuthorSays = ({ recipe }: { recipe: Recipe }) => {
+  return (
+    <div className="flex flex-col gap-[2px]">
+      <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[15px]">{recipe["who-are-you"].inputValue} says</p>
+      </span>
+      <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
+        <p className="text-[17px]">{recipe["who-are-you"].inputValue}</p>
+      </span>
+    </div>
+  );
+};
 
 const InfoBox = ({ recipe }: { recipe: Recipe }) => {
   return (
@@ -72,9 +162,9 @@ const InfoBox = ({ recipe }: { recipe: Recipe }) => {
         <h1 className="text-paper">{recipe["name-of-dish"].inputValue}</h1>
       </span>
       <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
-        <p className="text-paper text-[17px]">
+        <h2 className="text-paper text-[17px]">
           {recipe["who-are-you"].inputValue}
-        </p>
+        </h2>
       </span>
       <span className="bg-purple py-[20px] pl-[20px] pr-[40px]">
         <p className="text-salmon text-[17px]">
