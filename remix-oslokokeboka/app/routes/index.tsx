e4c3ec90f2157e18e7536a5f79d2ee7563253b10
Thusan Arul <thusan.arul@iterate.no>
2022-12-14 13:37:24 +0100

    Big orange button on root page redirects to form

--- remix-oslokokeboka/app/routes/index.tsx
+++ remix-oslokokeboka/app/routes/index.tsx
@@ -1,14 +1,20 @@
-// TODO
-// Create multistep Form first
-// Use prisma to save forms
-
+import { useNavigate } from "@remix-run/react";
 import { images } from "~/utils/title-images";
 
 export default function Index() {
+  const navigate = useNavigate();
+
   return (
     <div className="h-full justify-center flex flex-col gap-[52px] w-[816px] mx-auto">
       <div className="flex mx-auto justify-end w-full">
-        <button className="orange-button">Enter Oslo Kokeboka</button>
+        <button
+          onClick={() => {
+            navigate("/create-recipe/0");
+          }}
+          className="orange-button"
+        >
+          Enter Oslo Kokeboka
+        </button>
       </div>
       <div className="grid grid-cols-2 gap-[2px] mx-auto">
         {images.map((image, _) => (
