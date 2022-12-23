b21017e7dc5a7b069fb205f32fa87979370acb6b
Thusan Arul <thusan.arul@iterate.no>
2022-12-23 21:52:19 +0100

    Starts updating the landingpage with new design

--- remix-oslokokeboka/app/routes/index.tsx
+++ remix-oslokokeboka/app/routes/index.tsx
@@ -1,33 +1,25 @@
 import { useNavigate } from "@remix-run/react";
+import { useTranslation } from "react-i18next";
 import { images } from "~/utils/title-images";
 
 export default function Index() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   return (
-    <div className="h-full justify-center flex flex-col gap-[52px] w-[816px] mx-auto">
-      <div className="flex mx-auto justify-end w-full">
+    <div className="h-full justify-center flex flex-col gap-[52px] w-[85%] max-w-[600px] mx-auto">
+      <section id="what-is-kokeboka">
+        <h2 className="text-paper">{t("what-is-kokeboka")}</h2>
+      </section>
+      <div className="flex mx-auto justify-start w-full">
         <button
           onClick={() => {
             navigate("/create-recipe/0");
           }}
           className="orange-button"
         >
-          Enter Oslo Kokeboka
+          {t("submit-your-recipe")}
         </button>
-      </div>
-      <div className="grid grid-cols-2 gap-[2px] mx-auto">
-        {images.map((image, _) => (
-          <div
-            key={`img_${image.index}`}
-            className={`${image.bg_color} w-[408px] h-[85px] rounded-[6px] flex`}
-          >
-            <img
-              src={`images/titles/${image.index}.png`}
-              className={`mx-auto self-center mix-blend-multiply`}
-            />
-          </div>
-        ))}
       </div>
     </div>
   );
