ebd7d441eabeb8cc7437a5d69bbe3df723311a92
Thusan Arul <thusan.arul@iterate.no>
2023-01-17 20:34:37 +0100

    Adds entrypoint script that limits database connections
    - Stolen from headless-analytics

---
+++ remix-oslokokeboka/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -z "$DATABASE_CONNECTION_LIMIT" ]; then
+  export DATABASE_CONNECTION_LIMIT="4"
+  echo "Setting default connection limit ($DATABASE_CONNECTION_LIMIT)"
+fi
+
+# Naive, could break
+export DATABASE_URL="$DATABASE_URL&connection_limit=$DATABASE_CONNECTION_LIMIT"
+echo "Updated connection limit ($DATABASE_CONNECTION_LIMIT)"
+
+exec "$@"
