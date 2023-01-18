d66e778ce3e8b293622f6527db88bf97d8509fa8
Thusan Arul <thusan.arul@iterate.no>
2023-01-18 21:16:04 +0100

    Gets basic submission stats from db

--- remix-oslokokeboka/app/routes/_admin/index.tsx
+++ remix-oslokokeboka/app/routes/_admin/index.tsx
@@ -1,12 +1,41 @@
-import { RecipeSubmission } from "@prisma/client";
+import { RecipeSubmission, SubmissionState } from "@prisma/client";
 import { json, LoaderFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { db } from "~/utils/db.server";
 
+type SubmissionStats = {
+  total: number;
+  started: number;
+  completed: number;
+  processed: number;
+};
+
 export const loader: LoaderFunction = async ({}) => {
+  const submissionGroups = await db.recipeSubmission.groupBy({
+    by: ["state"],
+    _count: true,
+  });
+
+  const stats: SubmissionStats = {
+    total: submissionGroups.reduce(
+      (prevValue, group) => prevValue + group._count,
+      0
+    ),
+    started:
+      submissionGroups.find((v) => v.state === SubmissionState.STARTED)
+        ?._count ?? 0,
+    completed:
+      submissionGroups.find((v) => v.state === SubmissionState.COMPLETED)
+        ?._count ?? 0,
+    processed:
+      submissionGroups.find((v) => v.state === SubmissionState.PROCESSED)
+        ?._count ?? 0,
+  };
+
+  console.log(stats);
   const recipeSubmissions = await db.recipeSubmission.findMany({
     where: {
-      completed: true,
+      state: SubmissionState.COMPLETED,
     },
   });
 
