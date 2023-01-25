68c7beff7b7b49bdf8811b12122abfc4cc75a91f
Thusan Arul <thusan.arul@iterate.no>
2023-01-25 17:24:51 +0100

    Adds masonry style layout to submissions

--- remix-oslokokeboka/app/routes/_admin/index.tsx
+++ remix-oslokokeboka/app/routes/_admin/index.tsx
@@ -1,6 +1,7 @@
 import { RecipeSubmission, SubmissionState } from "@prisma/client";
 import { json, LoaderFunction } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { useMemo } from "react";
 import { db } from "~/utils/db.server";
 
 type SubmissionStats = {
@@ -86,34 +87,79 @@ export default function Admin() {
         </div>
       </section>
       <section id="submissions" className="flex flex-col gap-2">
-        {Object.keys(submissions).map((id, index) => {
-          return (
-            <Link
-              key={`submission-${index}`}
-              className="flex flex-col bg-darkwine max-w-[320px] p-5 gap-3"
-              to={`recipe/${id}`}
-            >
-              <h2 className="text-paper">
-                {submissions[id]["name-of-dish"] != ""
-                  ? submissions[id]["name-of-dish"]
-                  : "No dish name :("}
-              </h2>
-              <span className="flex gap-2">
-                <p className="text-ochre">
-                  {submissions[id]["name"] != ""
-                    ? submissions[id]["name"]
-                    : "No name :("}
-                </p>
-                <p>
-                  {submissions[id]["neighbourhood"] != ""
-                    ? submissions[id]["neighbourhood"]
-                    : "No neighbourhood :("}
-                </p>
-              </span>
-            </Link>
-          );
-        })}
+        <MasonrySubmissions submissions={submissions} />
       </section>
     </div>
   );
 }
+
+const MasonrySubmissions = ({ submissions }: { submissions: Submissions }) => {
+  const left = useMemo(
+    () => Object.keys(submissions).filter((_, i) => i % 2 !== 0),
+    [submissions]
+  );
+  const right = useMemo(
+    () => Object.keys(submissions).filter((_, i) => i % 2 === 0),
+    [submissions]
+  );
+
+  return (
+    <div className="flex gap-2">
+      <div className="flex flex-col gap-2">
+        {left.map((val, i) => {
+          return (
+            <Submission
+              key={"left-submission-" + i}
+              id={val}
+              submissions={submissions}
+            />
+          );
+        })}
+      </div>
+      <div className="flex flex-col gap-2">
+        {right.map((val, i) => {
+          return (
+            <Submission
+              key={"right-submission-" + i}
+              id={val}
+              submissions={submissions}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+const Submission = ({
+  id,
+  submissions,
+}: {
+  id: string;
+  submissions: Submissions;
+}) => {
+  return (
+    <Link
+      className="flex flex-col bg-darkwine w-[280px] p-5 gap-3"
+      to={`recipe/${id}`}
+    >
+      <h2 className="text-paper">
+        {submissions[id]["name-of-dish"] != ""
+          ? submissions[id]["name-of-dish"]
+          : "No dish name :("}
+      </h2>
+      <span className="flex gap-2">
+        <p className="text-ochre">
+          {submissions[id]["name"] != ""
+            ? submissions[id]["name"]
+            : "No name :("}
+        </p>
+        <p>
+          {submissions[id]["neighbourhood"] != ""
+            ? submissions[id]["neighbourhood"]
+            : "No neighbourhood :("}
+        </p>
+      </span>
+    </Link>
+  );
+};
