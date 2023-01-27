c354b66dacfd31d1beef1cb7865262201175c84e
Thusan Arul <thusan.arul@iterate.no>
2023-01-27 20:42:45 +0100

    Changes infobox components on home page to new fancy stuff

--- remix-oslokokeboka/app/routes/index.tsx
+++ remix-oslokokeboka/app/routes/index.tsx
@@ -1,4 +1,5 @@
 import { Link, useNavigate } from "@remix-run/react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Index() {
@@ -6,16 +7,18 @@ export default function Index() {
 
   const infoComponents = [
     {
-      id: "what-are-we-looking-for",
-      imgSrc: "images/food/pumpkin.png",
+      id: "why-are-we-doing-this",
+      imgSrc: "images/food/blue-kitchen.png",
       imgAlt: "pumpkin",
     },
     {
-      id: "why-are-we-doing-this",
-      imgSrc: "images/food/tuna.png",
+      id: "what-are-we-looking-for",
+      imgSrc: "images/food/pink-dining-room.png",
       imgAlt: "tuna",
     },
   ];
+
+  const [infoBoxVisible, setInfoBoxVisible] = useState<number>(0);
 
   return (
     <div className="flex flex-col gap-[52px] w-full md:w-[85%] md:max-w-[850px] md:mx-auto">
@@ -39,27 +42,31 @@ export default function Index() {
           </Link>
         </div>
       </section>
-      <div className="flex gap-[20px] px-home overflow-x-scroll scrollbar-hide md:justify-center">
-        {infoComponents.map((el, index) => (
-          <section
-            className="flex flex-col gap-[30px] md:w-[50%] md:max-w-[260px] data-[el=0]:pl-home data-[el=1]:pr-home"
-            id={el.id}
-            key={`info-${el.id}`}
-            data-el={index}
-          >
-            <h2 className="h-[64px] fuzzy text-paper text-center">
-              {t(`${el.id}-header`)}
-            </h2>
-            <img
-              className="min-w-[260px] max-w-[260px] h-[305px]"
-              src={el.imgSrc}
-              alt={el.imgAlt}
-            />
-            <p className="text-paper text-[12px] leading-[17px]">
-              {t(`${el.id}-text`)}
-            </p>
-          </section>
-        ))}
+      <div className="overflow-x-scroll scrollbar-hide h-[900px] mt-8 px-home">
+        <div className="flex md:justify-center">
+          {infoComponents.map((el, index) => (
+            <section
+              className="relative md:data-[el=0]:left-10 data-[visible='true']:z-10 data-[el=1]:right-10 data-[el=1]:top-8 data-[visible='false']:opacity-60"
+              id={el.id}
+              key={`info-${el.id}`}
+              data-el={index}
+              data-visible={index === infoBoxVisible}
+              onClick={() => {
+                setInfoBoxVisible(index);
+              }}
+            >
+              <h2 className="h-[64px] fuzzy text-paper text-center">
+                {t(`${el.id}-header`)}
+              </h2>
+              <div className="flex flex-col gap-[20px] bg-rectangle w-[335px] max-w-[335px] h-[651px]">
+                <img className="pt-8 px-2" alt={el.imgAlt} src={el.imgSrc} />
+                <p className="text-darkestwine px-4 pb-2">
+                  {t(`${el.id}-text`)}
+                </p>
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
     </div>
   );
