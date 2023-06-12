9584796fb68c5baf6e19c187c58e00f950c1b455
Thusan Arul <thusan.arul@iterate.no>
2023-06-12 16:17:58 +0200

    Adds scroll on click in modal

--- remix-oslokokeboka/app/components/recipe-info-modal.tsx
+++ remix-oslokokeboka/app/components/recipe-info-modal.tsx
@@ -1,5 +1,5 @@
 import { useLocation } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const info = [
@@ -49,12 +49,12 @@ const RenderInfoOverlay = ({}) => {
 
   const middle = `${scrollY}px`;
 
+  const { t } = useTranslation("recipe-info-modal");
+  const [currentElIndex, setCurrentElIndex] = useState(0);
+
   const style = {
     "--middle": middle,
   } as React.CSSProperties;
-
-  const { t } = useTranslation("recipe-info-modal");
-  const [currentElIndex, setCurrentElIndex] = useState(0);
 
   return (
     <section
@@ -63,10 +63,15 @@ const RenderInfoOverlay = ({}) => {
       className="text-white absolute z-30 w-[85%] max-w-[550px] bg-ochre rounded-lg flex flex-col gap-4 pl-5 py-4"
     >
       <h4 className="whitespace-nowrap w-full pr-5">{t("intro-header")}</h4>
-      <ul className="w-full flex gap-3 overflow-x scrollbar-hide">
+      <ul
+        data-scroll
+        style={{ "--move-to": currentElIndex } as React.CSSProperties}
+        className="w-full flex gap-3 overflow-x scrollbar-hide"
+      >
         {info.map((el, index) => {
           const isCurrentEl = currentElIndex === index;
           const isFirst = index === 0;
+
           return (
             <li
               key={index}
@@ -74,26 +79,28 @@ const RenderInfoOverlay = ({}) => {
                 !isCurrentEl ? "opacity-80" : ""
               }`}
             >
-              <div className="w-full flex gap-2 pl-5 pt-5 overflow-visible">
-                {isFirst ? (
-                  <>
-                    <span className="form-indicator w-[70%] bg-salmon" />
-                    <span className="form-indicator w-[30%]" />
-                  </>
-                ) : (
-                  <>
-                    <span className="form-indicator w-[15%]" />
-                    <span className="form-indicator w-[85%] bg-salmon" />
-                  </>
-                )}
-              </div>
-              <h3
-                className={`${
-                  isFirst ? "pl-5" : "pl-[55px]"
-                } fuzzy mt-3 text-[22px] text-salmon`}
-              >
-                {t(el.listTitle)}
-              </h3>
+              <button onClick={() => setCurrentElIndex(index)}>
+                <div className="w-full flex gap-2 pl-5 pt-5 overflow-visible">
+                  {isFirst ? (
+                    <>
+                      <span className="form-indicator w-[70%] bg-salmon" />
+                      <span className="form-indicator w-[30%]" />
+                    </>
+                  ) : (
+                    <>
+                      <span className="form-indicator w-[15%]" />
+                      <span className="form-indicator w-[85%] bg-salmon" />
+                    </>
+                  )}
+                </div>
+                <h3
+                  className={`${
+                    isFirst ? "pl-5" : "pl-[55px]"
+                  } fuzzy mt-3 text-[22px] text-salmon`}
+                >
+                  {t(el.listTitle)}
+                </h3>
+              </button>
             </li>
           );
         })}
